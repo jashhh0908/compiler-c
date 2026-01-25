6aae9315f7c6aabdf7b059ca199e2fdb7886f514
jash_nikumbhe <jashunik9@gmail.com>
2026-01-25 20:18:24 +0530

    implement file I/O based testing

--- main.c
+++ main.c
@@ -3,8 +3,26 @@
 #include "lexer.h"
 #include "parser.h"
 #include "ast.h"
+
+char *read_file() {
+    FILE* file = fopen("tests/input.txt", "rb");
+    if(!file) {
+        printf("Could not open\n");
+        return NULL;
+    }
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    char *buffer = malloc(size + 1);
+    fread(buffer, 1, size, file);
+    buffer[size] = '\0';
+    fclose(file);
+
+    return buffer;
+}
 int main(void) {
-    const char *test = "x = 10 + 2 * 3; print x;";
+    const char *test = read_file();
 
     init_lexer(test);
     ASTNode* success = parse_program();
